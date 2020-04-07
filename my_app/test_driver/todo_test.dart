7a6bedffa06cd8f200d29d343fdc5cc896b993dc
d.kanai <d.kanai@pastoraldog.com>
2020-04-07 23:26:12 +0900

    refactor web driver

--- my_app/test_driver/todo_test.dart
+++ my_app/test_driver/todo_test.dart
@@ -3,24 +3,22 @@ import 'package:test/test.dart';
 import 'web_driver.dart';
 
 void main() {
-  group('', () {
-    WebDriver driver = WebDriver();
+  WebDriver driver = WebDriver();
 
-    setUpAll(() async {
-      await driver.init();
-    });
+  setUpAll(() async {
+    await driver.init();
+  });
 
-    tearDownAll(() async {
-      driver.close();
-    });
+  tearDownAll(() async {
+    driver.close();
+  });
 
-    test('add new todo journy', () async {
-      await driver.containText("dummy toilet");
-      await driver.tap("RaisedButton");
-      await driver.containText("New Todo");
-      // fill in todo title
-      // click submit
-      // see new todo in list page
-    });
+  test('add new todo journy', () async {
+    await driver.containText("dummy toilet");
+    await driver.tap("RaisedButton");
+    await driver.containText("New Todo");
+    // fill in todo title
+    // click submit
+    // see new todo in list page
   });
 }
